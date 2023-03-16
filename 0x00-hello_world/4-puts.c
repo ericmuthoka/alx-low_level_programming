75414ed3a0db25dd97b1e783c8d2506c7b9137a4
ericmuthoka <muthokaerick@gmail.com>
2023-03-16 05:32:54 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts(""Programming is like building a multilingual puzzle")
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: should always be zero for success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
