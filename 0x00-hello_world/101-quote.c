6d9e784a652b08793a310c07eae9ccd2535df456
ericmuthoka <muthokaerick@gmail.com>
2023-03-16 16:28:28 -0700

    jjj

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always zero (success)
+ */
+
+int main(void)
+{
+	char char = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	putchar(char);
+	return (1);
+}
