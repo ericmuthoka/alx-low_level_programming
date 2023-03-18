b711b7d42b39a1dcf4c8183341aa0248d106e6a0
ericmuthoka <muthokaerick@gmail.com>
2023-03-17 18:53:38 -0700

    reverse alphabet with putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always zero
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
