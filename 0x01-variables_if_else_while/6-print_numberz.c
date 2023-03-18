9d0f397107d92c41d074633a5286185933a7b475
ericmuthoka <muthokaerick@gmail.com>
2023-03-17 18:28:43 -0700

    putchar for 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
+
