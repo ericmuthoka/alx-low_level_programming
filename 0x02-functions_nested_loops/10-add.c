75932551ca3d304ea54c2beb9062ca1a29af6378
ericmuthoka <muthokaerick@gmail.com>
2023-03-21 13:20:43 -0700

    summation is int add(int, int)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - a function that adds integers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: summation
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - a function that prints time table 
+ * Return: always zero
+ */
+
+void times_table(void)
+{
+	int i;
+
+}
