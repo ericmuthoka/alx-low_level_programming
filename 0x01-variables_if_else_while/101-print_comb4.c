43e07f04aa9ded49149642e5bc74799aadb8fc0a
ericmuthoka <muthokaerick@gmail.com>
2023-03-20 17:04:06 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: always zero
+ *
+ */
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+	for (y = 0; y <= 9; y++)
+	{
+	for (z = 0; z <= 9; z++)
+	{
+		if (x < y && y < z)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+			putchar(z + '0');
+		}
+	}
+	}
+	}
+		putchar(',');
+		putchar(' ');
+		return (0);
+}
