31df99c5c555c29ca835585807272c183c24cbbd
ericmuthoka <muthokaerick@gmail.com>
2023-03-23 05:47:10 -0700

    function that prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers -  a function that prints the numbers
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i >= 0; i <= 9; i++)
+		_putchar(i);
+	_putchar('\n');
+}
