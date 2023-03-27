c4e91a8104ca53b0d072d22349d4cf4b121da14c
ericmuthoka <muthokaerick@gmail.com>
2023-03-27 09:39:30 -0700

    src dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,19 +8,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int l = 0;
-	int x = 0;
+	int i;
 
-	while (*(src + l) != '\0')
-	{
-		l++;
-	}
-	for ( ; x < l ; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[l] = '\0';
+	for (i = 0; str[i] != '\0'; i++)
+		dest[i] = src[i];
 	return (dest);
+
 }
-
-
