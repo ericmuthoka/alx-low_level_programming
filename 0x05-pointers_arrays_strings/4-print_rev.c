6860ba7c25b815992db409d6c3e89cd4e39c9338
ericmuthoka <muthokaerick@gmail.com>
2023-03-28 12:08:16 -0700

    reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,8 +12,8 @@ void print_rev(char *s)
 	int len, i;
 	len = strlen(s);
 	
-	for (i = len - 1; s[i] >= 0; i--)
-		_putchar(*s[i]);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(*s);
 	_putchar('\n');
 
 }
