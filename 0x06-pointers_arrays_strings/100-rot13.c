c22b33599ea9d6296c376514ae739a4b5edd76be
ericmuthoka <muthokaerick@gmail.com>
2023-03-30 13:55:40 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,22 +11,15 @@
 char *rot13(char *s)
 {
 	int i;
-	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < 52; j++)
-		{
-			if (s[i] == data1[j])
-			{
-				s[i] = datarot[j];
-				break;
-			}
-		}
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = ((str[i] - 'a' + 13) % 26) + 'a';
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = ((str[i] - 'A' + 13) % 26) + 'A';
 	}
-	return (s);
+	return (str);
 }
 
 
