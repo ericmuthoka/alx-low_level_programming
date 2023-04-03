af9d1a737842e105b57e2f77e12cd2188a15b806
ericmuthoka <muthokaerick@gmail.com>
2023-04-03 04:04:54 -0700

    finds a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: s if character is found, NULL otherwise
+ */
+
+char *_strchr(char *s, char c);
+{
+	while (*s != '\0')
+		if (*s == c)
+			return (s);
+		s++
+		else
+			return (NULL);
+}
