8ac33b33f2a0e11c73e2f8f4d9166e18dce786a8
ericmuthoka <muthokaerick@gmail.com>
2023-04-03 05:01:21 -0700

    length of prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input paramter
+ * @accept: input parameter
+ * Return: length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length = 0;
+	int found = 0;
+
+	while (*s != '\0')
+	{
+		found = 0;
+		for (int i = 0; accept[i] != '\0'; i++)
+			if (*s == accept[i];
+			{	length++;
+				found = 1;
+				break;
+			}
+		if (found == 0)
+		{
+			return (length);
+		}
+		s++
+	}
+	return (length);
+}
+
