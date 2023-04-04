4da4d9dc47263d6b2ba1e3e58bc241fbbc852895
ericmuthoka <muthokaerick@gmail.com>
2023-04-03 17:49:45 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,17 +10,14 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
+		if (s[i] == c)
+			return (&s[i]);
 	}
-	if (*s == c)
-	{
-		return (s);
-	}
-	return (NULL);
+	return (0);
 }
+
+
