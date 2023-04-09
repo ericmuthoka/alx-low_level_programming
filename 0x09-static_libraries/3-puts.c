65e083d525bd7e3984ce7b131892356eb036ebf8
ericmuthoka <muthokaerick@gmail.com>
2023-04-09 10:32:43 -0700

    libmy.a

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -7,13 +7,11 @@
  *
  * Return: void
  */
-
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str + 0);
-		++str;
-	}
-	_putchar('\n');
+    while (*str != '\0')
+    {
+        _putchar(*str++);
+    }
+    _putchar('\n');
 }
