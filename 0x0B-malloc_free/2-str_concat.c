8392580a1583b97afaa11bc49ee73e804605a607
ericmuthoka <muthokaerick@gmail.com>
2023-04-11 09:13:10 -0700

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,9 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		s1 = ""; /* using strlen on NULL string results in undefined behavior */
+			 /* to avoid this, an empty string is assigned to s1 and s2 */
+			/* to ensure the function can safely find the length of the strings */
 	}
 	if (s2 == NULL)
 	{
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+#include <stdlib>
+
+/**
+ * alloc_grid -  returns a pointer to a 2 dimensional integer array
+ * @width: one dimension
+ * @height: second dimension
+ * Return: pointer to the 2-d integer array, NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+
+}
