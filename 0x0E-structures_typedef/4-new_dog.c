bcaf3f2fd047be05e9f67245972ba8068f88d456
ericmuthoka <muthokaerick@gmail.com>
2023-04-17 06:03:40 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,38 +1,46 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 #include <string.h>
 /**
- * new_dog - creates a new dog.
+ * new_dog - function that creates a new dog
+ *
  * @name: dog name
  * @age: dog age
- * @owner: dog owner name
+ * @owner: dog owner
+ *
+ * Return: struct pointer or NULL for fail
  */
-typedef struct {
-    char* name;
-    float age;
-    char* owner;
-} dog_t;
 
-dog_t* new_dog(char* name, float age, char* owner) {
-    dog_t* new_dog = malloc(sizeof(dog_t));
-    if (!new_dog) {
-        return NULL; // memory allocation failed
-    }
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
 
-    // allocate memory for name and owner strings
-    new_dog->name = malloc(strlen(name) + 1);
-    new_dog->owner = malloc(strlen(owner) + 1);
-    if (!new_dog->name || !new_dog->owner) {
-        free(new_dog); // free the previously allocated memory
-        return NULL; // memory allocation failed
-    }
+	/* if name and owner are empty and age < 0 return null*/
+	if (!name || age < 0 || !owner)
+		return (NULL);
 
-    // copy the name and owner strings
-    strcpy(new_dog->name, name);
-    strcpy(new_dog->owner, owner);
+	dog = (dog_t *) malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 
-    new_dog->age = age;
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 
-    return new_dog;
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if ((*dog).owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = strcopy(dog->name, name);
+	dog->age = age;
+	dog->owner = strcopy(dog->owner, owner);
+
+	return (dog);
 }
-
