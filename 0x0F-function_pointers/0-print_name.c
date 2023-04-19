b4d55260924ada4aa31a0d7a5314e1a2d3d4f86a
ericmuthoka <muthokaerick@gmail.com>
2023-04-19 07:23:25 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints name
+ * @name: name
+ * @f: function pointer
+ * Return: Always 0 (Success)
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
