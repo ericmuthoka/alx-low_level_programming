2606dfea35ad285e213a269ab085ac0c9f700a20
ericmuthoka <muthokaerick@gmail.com>
2023-04-20 10:25:33 -0700

    returns the sum of fn parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - a function that returns the sum of all its parameters
+ * @n: number of variable arguments that follow
+ * @...: variadic
+ * Return:sum of function parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum;
+
+	sum = 0;
+	va_start(ap, n); /* Initialize the argument list */
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int); /* same as sum = sum + va_arg(ap, int); */
+	}
+	va_end(ap);
+	return (sum);
+}
+
+
