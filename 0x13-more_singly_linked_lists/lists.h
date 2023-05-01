30eb71579fd757de85ced9635565d17e4900af3c
ericmuthoka <muthokaerick@gmail.com>
2023-04-30 23:38:26 -0700

    sinly linked

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,11 +17,6 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-
-#include <stddef.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
