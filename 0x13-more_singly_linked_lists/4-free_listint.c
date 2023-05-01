ebe1ebc9feeaac2638836a329d6b95da6d4edac2
ericmuthoka <muthokaerick@gmail.com>
2023-04-30 23:35:12 -0700

    sinly linked

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * free_listint - Frees a linked list.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	/* Declare a temporary pointer to hold the current node */
+	listint_t *current;
+
+	/* Loop until the end of the list is reached */
+	while (head != NULL)
+	{
+		/* Save the current node so that we can free it later */
+		current = head;
+
+		/* Update the head pointer to point to the next node in the list */
+		head = head->next;
+
+		/* Free the current node */
+		free(current);
+	}
+}
+
