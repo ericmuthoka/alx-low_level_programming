279b83e5a6e6b61ea332dda677707daab5fd06ea
ericmuthoka <muthokaerick@gmail.com>
2023-05-02 03:42:28 -0700

    more singly lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,25 +9,41 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *new_node, *prev_node, *current_node;
+	unsigned int i;
+
 	if (head == NULL)
 		return (NULL);
 
 	if (idx == 0)
 		return (add_nodeint(head, n));
 
-	listint_t *prev = get_nodeint_at_index(*head, idx - 1);
+	current_node = *head;
+	for (i = 0; current_node != NULL && i < idx; i++)
+	{
+		prev_node = current_node;
+		current_node = current_node->next;
+	}
 
-	if (prev == NULL)
+	if (i < idx)
 		return (NULL);
 
-	listint_t *new_node = malloc(sizeof(*new_node));
+	new_node = malloc(sizeof(*new_node));
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = prev->next;
-	prev->next = new_node;
+	if (current_node == *head)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		new_node->next = current_node;
+		prev_node->next = new_node;
+	}
 
 	return (new_node);
 }
