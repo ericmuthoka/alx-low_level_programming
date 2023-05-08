db3900f89178198c9c1bb62604328de99e2e8f9f
ericmuthoka <muthokaerick@gmail.com>
2023-05-08 09:37:26 -0700

    file manipulation

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include <main.h>
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the name of the file
+ * @text_content: the text to add to the end of the file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	if (filename == NULL)
+		return (-1);
+
+	FILE *fp = fopen(filename, "a");
+
+	if (fp == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		size_t len = strlen(text_content);
+
+		if (fwrite(text_content, sizeof(char), len, fp) != len)
+		{
+			fclose(fp);
+			return (-1);
+		}
+	}
+
+	fclose(fp);
+	return (1);
+}
+
